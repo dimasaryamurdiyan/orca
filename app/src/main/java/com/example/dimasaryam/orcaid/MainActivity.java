0919f3edc8628868299de2e1198d41b4d96691be
DESKTOP-78512H6\Dimas Arya M <dimasaryamurdiyan123@gmail.com>
2018-11-21 14:51:51 +0700

    new progress

--- app/src/main/java/com/example/dimasaryam/orcaid/MainActivity.java
+++ app/src/main/java/com/example/dimasaryam/orcaid/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.dimasaryam.orcaid;
 
+import android.content.Intent;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppBarLayout myAppbar = (AppBarLayout) findViewById(R.id.appbar);
+        setSupportActionBar(myAppbar);
+        ImageButton img1 = (ImageButton) findViewById(R.id.picbut1);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, polygone.class);
+                startActivity(intent);
+            }
+        });
+        ImageButton img2 = (ImageButton) findViewById(R.id.picbut2);
+        ImageButton img3 = (ImageButton) findViewById(R.id.picbut3);
+        ImageButton img4 = (ImageButton) findViewById(R.id.picbut4);
+        img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Movers.class);
+                startActivity(i);
+            }
+        });
+        ImageButton img5 = (ImageButton) findViewById(R.id.picbut5);
+        ImageButton img6 = (ImageButton) findViewById(R.id.picbut6);
+    }
+
+    private void setSupportActionBar(AppBarLayout myAppbar) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/dimasaryam/orcaid/polygone.java
@@ -0,0 +1,16 @@
+package com.example.dimasaryam.orcaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class polygone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_polygone);
+
+
+    }
+}
